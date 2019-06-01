3d017f2f7e17f91c9e56b43169d9fcaa2667eb3b
Marco <marcotrumpet90@gmail.com>
2019-06-01 16:20:27 +0200

    final

--- flutter_shirt/lib/homePage/home_page.dart
+++ flutter_shirt/lib/homePage/home_page.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-//import 'package:camera/camera.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,12 +14,17 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn googleUser = GoogleSignIn();
+  File cameraFile;
 
   void _signOut() {
     _auth.signOut().then((_) {
 //      googleUser.signOut();
       Navigator.of(context).pop();
     });
+  }
+
+  Map<String, dynamic> toMap(String url, String uid) {
+    return {'url': url, 'uid': uid};
   }
 
   @override
@@ -35,18 +43,68 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Center(
-            child: Text('ciao'),
-          ),
-        ],
+      body: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection('photos').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return new Text('Loading...');
+            default:
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  String url = snapshot.data.documents[index].data["url"];
+                  return FutureBuilder(
+                    future: FirebaseStorage.instance
+                        .ref()
+                        .child(url)
+                        .getDownloadURL(),
+                    builder: (context, snapshot2) {
+                      if (snapshot2.hasData) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width - 60,
+                          height: MediaQuery.of(context).size.width - 60,
+                          child: Center(
+                            child: Card(
+                              elevation: 2,
+                              child: Column(
+                                children: <Widget>[
+                                  Image.network(snapshot2.data),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      } else {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                    },
+                  );
+                },
+                itemCount: snapshot.data.documents.length,
+              );
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-
+        onPressed: () async {
+          cameraFile = await ImagePicker.pickImage(
+            source: ImageSource.camera,
+            maxHeight: 50.0,
+            maxWidth: 50.0,
+          );
+          print("You selected camera image : " + cameraFile.path);
+          FirebaseUser user = await _auth.currentUser();
+          String name =
+              DateTime.now().millisecondsSinceEpoch.toString() + '.jpg';
+          final StorageReference firebaseStorageRef =
+              FirebaseStorage.instance.ref().child(user.uid).child(name);
+          final StorageUploadTask task = firebaseStorageRef.putFile(cameraFile);
+          Firestore.instance
+              .collection('photos')
+              .document(DateTime.now().millisecondsSinceEpoch.toString())
+              .setData(toMap("${user.uid}/${name}", user.uid));
+          setState(() {});
         },
         child: Icon(Icons.camera_alt),
       ),
