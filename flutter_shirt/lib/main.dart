bb858a641ec9034f8cc984541740261a114af0c9
Marco <marcotrumpet90@gmail.com>
2019-06-01 11:41:48 +0200

    title

--- flutter_shirt/lib/main.dart
+++ flutter_shirt/lib/main.dart
@@ -30,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Flutter_Shirt"),
       ),
       body: Center(
         child: RootPage(),
